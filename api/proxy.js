a6a738ea597a2e069e9e52a61198c2827c46ba17
Odin Luo <Odin.Luo@everymatrix.com>
2023-06-09 18:03:51 +0800

    vercel proxy

---
+++ api/proxy.js
@@ -0,0 +1,18 @@
+const {
+    createProxyMiddleware
+} = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+    let target = ''
+    if (req.url.startsWith('/api')) {
+        target = 'https://2504.qiangtu.com:5231'
+    }
+
+    createProxyMiddleware({
+        target,
+        changeOrigin: true,
+        // pathRewrite: {
+        //     '^/api/': '/'
+        // }
+    })(req, res)
+}
