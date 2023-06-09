9839fa7c0aabc0b89b121dc08643ff980f4366d4
Odin Luo <Odin.Luo@everymatrix.com>
2023-06-09 18:30:41 +0800

    反代/

--- web/api/proxy.js
+++ web/api/proxy.js
@@ -3,16 +3,17 @@ const {
 } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-    let target = ''
-    if (req.url.startsWith('/api')) {
-        target = 'https://2504.qiangtu.com:5231'
-    }
+    let target = 'https://2504.qiangtu.com:5231'
+    // if (req.url.startsWith('/api') || 
+    // req.url.startsWith('/u/')) {
+    //     target = 'https://2504.qiangtu.com:5231'
+    // }
 
     createProxyMiddleware({
         target,
         changeOrigin: true,
-        // pathRewrite: {
-        //     '^/api/': '/'
-        // }
+        pathRewrite: {
+            '^/': '/'
+        }
     })(req, res)
 }
